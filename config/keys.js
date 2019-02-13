7374948f9686e6c3e69a7d26a8ef23823827bede
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-02-13 02:23:32 +0100

    Initial express server with routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://jota:jota@devconnector-shard-00-00-qaclp.mongodb.net:27017,devconnector-shard-00-01-qaclp.mongodb.net:27017,devconnector-shard-00-02-qaclp.mongodb.net:27017/devconnector?ssl=true&replicaSet=devconnector-shard-0&authSource=admin&retryWrites=true"
+}
