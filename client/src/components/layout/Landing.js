bdb7e180c7a410c303c656cc9851e34ac55c57d4
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-02-19 12:51:16 +0100

    Section 7 completed

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import { Link } from 'react-router-dom';
+
 class Landing extends Component {
   render() {
     return (
@@ -11,8 +13,8 @@ class Landing extends Component {
                             <h1 class="display-3 mb-4">Developer Connector</h1>
                             <p class="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
                             <hr />
-                            <a href="register.html" class="btn btn-lg btn-info mr-2">Sign Up</a>
-                            <a href="login.html" class="btn btn-lg btn-light">Login</a>
+                            <Link to="/register" class="btn btn-lg btn-info mr-2">Sign Up</Link>
+                            <Link to="/login" class="btn btn-lg btn-light">Login</Link>
                         </div>
                     </div>
                 </div>
