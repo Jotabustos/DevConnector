2bee2aa4141fd2d7e770bb140428f805fa0bf5b4
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-02-20 12:28:51 +0100

    Section 8 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import jwt_decode from 'jwt-decode';
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logOutUser } from "./actions/authActions";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -8,20 +14,44 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Check for the token
+if(localStorage.jwtToken){
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and is Authenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    //Logout the user
+    store.dispatch(logOutUser());
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = '/login'
+  }
+}
+
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
