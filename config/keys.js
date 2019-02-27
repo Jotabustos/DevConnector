dc4e697ad2d388e6a9420f764f2ac79160938c21
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-02-27 17:46:31 +0100

    Production

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: "mongodb://jota:jota@devconnector-shard-00-00-qaclp.mongodb.net:27017,devconnector-shard-00-01-qaclp.mongodb.net:27017,devconnector-shard-00-02-qaclp.mongodb.net:27017/devconnector?ssl=true&replicaSet=devconnector-shard-0&authSource=admin&retryWrites=true",
-    secretOrKey: 'RandomString'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
